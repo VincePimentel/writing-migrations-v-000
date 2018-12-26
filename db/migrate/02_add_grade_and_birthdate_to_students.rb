1d34c58f71a0d8ece19be49edd1d06aaccda182c
Vince Andrew Pimentel <contact@vincep.me>
2018-12-26 02:25:35 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,5 +1,5 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[4.2]
   def change
-    add_column :grade, :integer, :birthdate, :string
+    add_column :grade, :birthdate, :integer, :string
   end
 end
